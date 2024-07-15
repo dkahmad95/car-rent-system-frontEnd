ef5dbd82cd7ae4f9fbb487f5b39ef9d08a1b1914
dkahmad95 <dkahmad95@gmail.com>
2024-07-15 13:31:13 +0300

    3rd commit

--- src/ui/sharedComponents/Footer.tsx
+++ src/ui/sharedComponents/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-stone-200 text-center py-4">
       <div className="flex justify-center items-center">
         <p className="text-sm">
-          &copy; 2024 Bleik Car Rent. All rights reserved.
+          &copy; Ahmad Dekmak. All rights reserved.
         </p>
       </div>
     </footer>
